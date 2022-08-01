e6773d224e421766f34d012c0062f7a7f3790273
Thomas Nicollet <thomas.nicollet@nebulis.io>
2022-08-01 23:48:17 +0200

    :sparkles: Added action

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const http = require("@actions/http-client");
+const auth = require("@actions/http-client/lib/auth");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const host = core.getInput("host");
+    const organization = core.getInput("organization");
+    const workspace = core.getInput("workspace");
+    const token = core.getInput("token");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const url = `https://${host}/api/v2/organizations/${organization}/workspaces/${workspace}?include=outputs`;
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info(`Connecting to ${url}`);
+
+    const bearerToken = new auth.BearerCredentialHandler(token);
+
+    const httpClient = new http.HttpClient("terraform-outputs", [bearerToken]);
+
+    const headers = { [http.Headers.ContentType]: "application/vnd.api+json" };
+
+    const res = await httpClient.get(`${url}`, headers);
+
+    const { errors, included } = JSON.parse(await res.readBody());
+
+    if (errors !== undefined) {
+      core.setFailed(JSON.stringify(errors));
+    }
+
+    let outputs = included.reduce((acc, x) => {
+      acc[x.attributes.name] = x.attributes.value;
+      return acc;
+    }, {});
+
+    core.setOutput("outputs", `'${JSON.stringify(outputs)}'`);
   } catch (error) {
     core.setFailed(error.message);
   }
